03bbfe9810a665c7a9bbd7f2375d36f8ad60cd2b
Ricardo Barbosa <ricardo.barbosa@waycarbon.com>
2021-02-28 09:48:41 -0300

    Refatorando o Teste de Unidade

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -7,35 +7,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-class ListAndItemModelsTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        my_list = List()
-        my_list.save()
-
-        first_item = Item()
-        first_item.text = 'O primeiro item'
-        first_item.list = my_list
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'O segundo item'
-        second_item.list = my_list
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, my_list)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'O primeiro item')
-        self.assertEqual(first_saved_item.list, my_list)
-        self.assertEqual(second_saved_item.text, 'O segundo item')
-        self.assertEqual(second_saved_item.list, my_list)
-
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
@@ -78,6 +49,8 @@ class NewListTest(TestCase):
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
 
+class NewItemTest(TestCase):
+
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
@@ -91,7 +64,6 @@ class NewListTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
-
 
     def test_redirects_to_list_view(self):
         other_list = List.objects.create()
